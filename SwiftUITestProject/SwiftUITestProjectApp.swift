830395d3c7445c4e60b12f0566e5060ee9c19962
Nikita Kosolapov <kosolapovnikitav@gmail.com>
2023-09-30 22:59:17 +0300

    Initial Commit

---
+++ SwiftUITestProject/SwiftUITestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITestProjectApp.swift
+//  SwiftUITestProject
+//
+//  Created by Nikita Kosolapov on 30.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

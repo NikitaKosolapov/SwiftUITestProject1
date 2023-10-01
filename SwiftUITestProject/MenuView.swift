14498bee40d9ba9c200413d995e64d1762173c76
Nikita Kosolapov <kosolapovnikitav@gmail.com>
2023-10-01 10:33:13 +0300

    Finished menu view

--- SwiftUITestProject/MenuView.swift
+++ SwiftUITestProject/MenuView.swift
@@ -13,16 +13,38 @@ struct MenuView: View {
             Spacer()
 
             VStack(spacing: 16) {
+                Text("Meng - 28% complete")
+                    .font(.caption)
+                
+                Color.white
+                    .frame(width: 38, height: 6)
+                    .cornerRadius(3)
+                    .frame(width: 130, height: 6, alignment: .leading)
+                    .background(.black.opacity(0.08))
+                    .cornerRadius(3)
+                    .padding()
+                    .frame(width: 150, height: 24)
+                    .background(.black.opacity(0.1))
+                    .cornerRadius(12)
+                
                 MenuRow(title: "Account", icon: "gear")
                 MenuRow(title: "Billing", icon: "creditcard")
                 MenuRow(title: "Sign out", icon: "person.crop.circle")
             }
             .frame(maxWidth: .infinity)
             .frame(height: 300)
-            .background(.white)
+            .background(LinearGradient(gradient: Gradient(colors: [.white, .menu]), startPoint: .top, endPoint: .bottom))
             .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
-            .shadow(radius: 30)
-        .padding(.horizontal, 30)
+            .shadow(color: .black.opacity(0.2), radius: 20, x: 0, y: 20)
+            .padding(.horizontal, 30)
+            .overlay(alignment: .center, content: {
+                Image(.avatar)
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 60, height: 60)
+                    .clipShape(Circle())
+                    .offset(y: -150)
+            })
         }
         .padding(.bottom, 30)
     }
@@ -42,6 +64,7 @@ struct MenuRow: View {
                 .font(.system(size: 20, weight: .light))
                 .imageScale(.large)
                 .frame(width: 32, height: 32)
+                .foregroundColor(.icon)
             
             Text(title)
                 .font(.system(size: 20, weight: .bold, design: .rounded))

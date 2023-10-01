aa6fa2ea6d12499d26679edc09cfb5475ded948c
Nikita Kosolapov <kosolapovnikitav@gmail.com>
2023-10-01 07:34:18 +0300

    FirstCommit

---
+++ DerivedData/SwiftUITestProject/Build/Intermediates.noindex/Previews/SwiftUITestProject/Intermediates.noindex/SwiftUITestProject.build/Debug-iphonesimulator/SwiftUITestProject.build/Objects-normal/arm64/ContentView.1.preview-thunk.swift
@@ -0,0 +1,156 @@
+@_private(sourceFile: "ContentView.swift") import SwiftUITestProject
+import func SwiftUI.__designTimeBoolean
+import func SwiftUI.__designTimeInteger
+import protocol SwiftUI.PreviewProvider
+import func SwiftUI.__designTimeString
+import func SwiftUI.__designTimeFloat
+import struct SwiftUI.EmptyView
+import protocol SwiftUI.View
+import SwiftUI
+
+extension BottomCardView {
+    @_dynamicReplacement(for: body) private var __preview__body: some View {
+        #sourceLocation(file: "/Users/Nikita_1/Desktop/MyLife/Programming/Projects/SUI/SwiftUITestProject/SwiftUITestProject/ContentView.swift", line: 115)
+        VStack(spacing: __designTimeInteger("#1730.[6].[0].property.[0].[0].arg[0].value", fallback: 20)) {
+            Rectangle()
+                .frame(width: __designTimeInteger("#1730.[6].[0].property.[0].[0].arg[1].value.[0].modifier[0].arg[0].value", fallback: 40), height: __designTimeInteger("#1730.[6].[0].property.[0].[0].arg[1].value.[0].modifier[0].arg[1].value", fallback: 5))
+                .cornerRadius(__designTimeInteger("#1730.[6].[0].property.[0].[0].arg[1].value.[0].modifier[1].arg[0].value", fallback: 3))
+                .opacity(__designTimeFloat("#1730.[6].[0].property.[0].[0].arg[1].value.[0].modifier[2].arg[0].value", fallback: 0.1))
+            Text(__designTimeString("#1730.[6].[0].property.[0].[0].arg[1].value.[1].arg[0].value", fallback: "This certificate is proof that Meng To has achieved the UI Design course with approval from a Design+Code instructor."))
+                .multilineTextAlignment(.center)
+                .font(.subheadline)
+                .lineSpacing(__designTimeInteger("#1730.[6].[0].property.[0].[0].arg[1].value.[1].modifier[2].arg[0].value", fallback: 4))
+            Spacer()
+        }
+        .padding(.top, __designTimeInteger("#1730.[6].[0].property.[0].[0].modifier[0].arg[1].value", fallback: 8))
+        .padding(.horizontal, __designTimeInteger("#1730.[6].[0].property.[0].[0].modifier[1].arg[1].value", fallback: 20))
+        .frame(maxWidth: .infinity)
+        .background(.white)
+        .cornerRadius(__designTimeInteger("#1730.[6].[0].property.[0].[0].modifier[4].arg[0].value", fallback: 30))
+        .shadow(radius: __designTimeInteger("#1730.[6].[0].property.[0].[0].modifier[5].arg[0].value", fallback: 20))
+        .offset(x: __designTimeInteger("#1730.[6].[0].property.[0].[0].modifier[6].arg[0].value", fallback: 0), y: __designTimeInteger("#1730.[6].[0].property.[0].[0].modifier[6].arg[1].value", fallback: 500))
+    
+#sourceLocation()
+    }
+}
+
+extension TitleView {
+    @_dynamicReplacement(for: body) private var __preview__body: some View {
+        #sourceLocation(file: "/Users/Nikita_1/Desktop/MyLife/Programming/Projects/SUI/SwiftUITestProject/SwiftUITestProject/ContentView.swift", line: 99)
+        VStack {
+            HStack {
+                Text(__designTimeString("#1730.[5].[0].property.[0].[0].arg[0].value.[0].arg[0].value.[0].arg[0].value", fallback: "Certificates"))
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                Spacer()
+            }
+            .padding()
+            Image(.background1)
+            Spacer()
+        }
+    
+#sourceLocation()
+    }
+}
+
+extension BackCardView {
+    @_dynamicReplacement(for: body) private var __preview__body: some View {
+        #sourceLocation(file: "/Users/Nikita_1/Desktop/MyLife/Programming/Projects/SUI/SwiftUITestProject/SwiftUITestProject/ContentView.swift", line: 90)
+        VStack {
+            Spacer()
+        }
+        .frame(width: __designTimeInteger("#1730.[4].[0].property.[0].[0].modifier[0].arg[0].value", fallback: 340), height: __designTimeInteger("#1730.[4].[0].property.[0].[0].modifier[0].arg[1].value", fallback: 220))
+    
+#sourceLocation()
+    }
+}
+
+extension CardView {
+    @_dynamicReplacement(for: body) private var __preview__body: some View {
+        #sourceLocation(file: "/Users/Nikita_1/Desktop/MyLife/Programming/Projects/SUI/SwiftUITestProject/SwiftUITestProject/ContentView.swift", line: 60)
+        VStack {
+            HStack {
+                VStack(alignment: .leading) {
+                    Text(__designTimeString("#1730.[3].[0].property.[0].[0].arg[0].value.[0].arg[0].value.[0].arg[1].value.[0].arg[0].value", fallback: "UI Design"))
+                        .font(.title)
+                        .fontWeight(.semibold)
+                        .foregroundStyle(.white)
+                    Text(__designTimeString("#1730.[3].[0].property.[0].[0].arg[0].value.[0].arg[0].value.[0].arg[1].value.[1].arg[0].value", fallback: "Certificate"))
+                        .foregroundColor(Color(__designTimeString("#1730.[3].[0].property.[0].[0].arg[0].value.[0].arg[0].value.[0].arg[1].value.[1].modifier[0].arg[0].value.arg[0].value", fallback: "accent")))
+                }
+                Spacer()
+                Image(__designTimeString("#1730.[3].[0].property.[0].[0].arg[0].value.[0].arg[0].value.[2].arg[0].value", fallback: "Logo1"))
+            }
+            .padding(.horizontal, __designTimeInteger("#1730.[3].[0].property.[0].[0].arg[0].value.[0].modifier[0].arg[1].value", fallback: 20))
+            .padding(.top, __designTimeInteger("#1730.[3].[0].property.[0].[0].arg[0].value.[0].modifier[1].arg[1].value", fallback: 20))
+            Spacer()
+            Image(__designTimeString("#1730.[3].[0].property.[0].[0].arg[0].value.[2].arg[0].value", fallback: "Card1"))
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: __designTimeInteger("#1730.[3].[0].property.[0].[0].arg[0].value.[2].modifier[2].arg[0].value", fallback: 300), height: __designTimeInteger("#1730.[3].[0].property.[0].[0].arg[0].value.[2].modifier[2].arg[1].value", fallback: 110), alignment: .top)
+        }
+        .frame(width: __designTimeFloat("#1730.[3].[0].property.[0].[0].modifier[0].arg[0].value", fallback: 340.0), height: __designTimeFloat("#1730.[3].[0].property.[0].[0].modifier[0].arg[1].value", fallback: 220.0))
+        .background(.black)
+        .cornerRadius(__designTimeInteger("#1730.[3].[0].property.[0].[0].modifier[2].arg[0].value", fallback: 20))
+        .shadow(radius: __designTimeInteger("#1730.[3].[0].property.[0].[0].modifier[3].arg[0].value", fallback: 20))
+    
+#sourceLocation()
+    }
+}
+
+extension ContentView {
+    @_dynamicReplacement(for: body) private var __preview__body: some View {
+        #sourceLocation(file: "/Users/Nikita_1/Desktop/MyLife/Programming/Projects/SUI/SwiftUITestProject/SwiftUITestProject/ContentView.swift", line: 14)
+        ZStack {
+            TitleView()
+                .blur(radius: show ? __designTimeInteger("#1730.[1].[1].property.[0].[0].arg[0].value.[0].modifier[0].arg[0].value.then", fallback: 20) : __designTimeInteger("#1730.[1].[1].property.[0].[0].arg[0].value.[0].modifier[0].arg[0].value.else", fallback: 0))
+                .animation(.easeInOut, value: show)
+            
+            BackCardView()
+                .background(show ? .card3 : .card4)
+                .cornerRadius(__designTimeInteger("#1730.[1].[1].property.[0].[0].arg[0].value.[1].modifier[1].arg[0].value", fallback: 20))
+                .shadow(radius: __designTimeInteger("#1730.[1].[1].property.[0].[0].arg[0].value.[1].modifier[2].arg[0].value", fallback: 20))
+                .offset(x: __designTimeInteger("#1730.[1].[1].property.[0].[0].arg[0].value.[1].modifier[3].arg[0].value", fallback: 0), y: show ? __designTimeInteger("#1730.[1].[1].property.[0].[0].arg[0].value.[1].modifier[3].arg[1].value.then", fallback: -400) : __designTimeInteger("#1730.[1].[1].property.[0].[0].arg[0].value.[1].modifier[3].arg[1].value.else", fallback: -40))
+                .scaleEffect(__designTimeFloat("#1730.[1].[1].property.[0].[0].arg[0].value.[1].modifier[4].arg[0].value", fallback: 0.9))
+                .rotationEffect(.degrees(show ? __designTimeInteger("#1730.[1].[1].property.[0].[0].arg[0].value.[1].modifier[5].arg[0].value.arg[0].value.then", fallback: 0) : __designTimeInteger("#1730.[1].[1].property.[0].[0].arg[0].value.[1].modifier[5].arg[0].value.arg[0].value.else", fallback: 10)))
+                .rotation3DEffect(.degrees(__designTimeInteger("#1730.[1].[1].property.[0].[0].arg[0].value.[1].modifier[6].arg[0].value.arg[0].value", fallback: 10)), axis: (x: __designTimeFloat("#1730.[1].[1].property.[0].[0].arg[0].value.[1].modifier[6].arg[1].value.[0]", fallback: 10.0), y: __designTimeFloat("#1730.[1].[1].property.[0].[0].arg[0].value.[1].modifier[6].arg[1].value.[1]", fallback: 0.0), z: __designTimeFloat("#1730.[1].[1].property.[0].[0].arg[0].value.[1].modifier[6].arg[1].value.[2]", fallback: 0.0)))
+                .blendMode(.hardLight)
+                .animation(.easeInOut(duration: __designTimeFloat("#1730.[1].[1].property.[0].[0].arg[0].value.[1].modifier[8].arg[0].value.arg[0].value", fallback: 0.5)), value: show)
+            
+            BackCardView()
+                .background(show ? .card4 : .card3)
+                .cornerRadius(__designTimeInteger("#1730.[1].[1].property.[0].[0].arg[0].value.[2].modifier[1].arg[0].value", fallback: 20))
+                .shadow(radius: __designTimeInteger("#1730.[1].[1].property.[0].[0].arg[0].value.[2].modifier[2].arg[0].value", fallback: 20))
+                .offset(x: __designTimeInteger("#1730.[1].[1].property.[0].[0].arg[0].value.[2].modifier[3].arg[0].value", fallback: 0), y: show ? __designTimeInteger("#1730.[1].[1].property.[0].[0].arg[0].value.[2].modifier[3].arg[1].value.then", fallback: -200) : __designTimeInteger("#1730.[1].[1].property.[0].[0].arg[0].value.[2].modifier[3].arg[1].value.else", fallback: -20))
+                .scaleEffect(__designTimeFloat("#1730.[1].[1].property.[0].[0].arg[0].value.[2].modifier[4].arg[0].value", fallback: 0.95))
+                .rotationEffect(.degrees(show ? __designTimeInteger("#1730.[1].[1].property.[0].[0].arg[0].value.[2].modifier[5].arg[0].value.arg[0].value.then", fallback: 0) : __designTimeInteger("#1730.[1].[1].property.[0].[0].arg[0].value.[2].modifier[5].arg[0].value.arg[0].value.else", fallback: 5)))
+                .rotation3DEffect(.degrees(__designTimeInteger("#1730.[1].[1].property.[0].[0].arg[0].value.[2].modifier[6].arg[0].value.arg[0].value", fallback: 5)), axis: (x: __designTimeFloat("#1730.[1].[1].property.[0].[0].arg[0].value.[2].modifier[6].arg[1].value.[0]", fallback: 10.0), y: __designTimeFloat("#1730.[1].[1].property.[0].[0].arg[0].value.[2].modifier[6].arg[1].value.[1]", fallback: 0.0), z: __designTimeFloat("#1730.[1].[1].property.[0].[0].arg[0].value.[2].modifier[6].arg[1].value.[2]", fallback: 0.0)))
+                .blendMode(.hardLight)
+                .animation(.easeInOut(duration: __designTimeFloat("#1730.[1].[1].property.[0].[0].arg[0].value.[2].modifier[8].arg[0].value.arg[0].value", fallback: 0.3)), value: show)
+            
+            CardView()
+                .blendMode(.hardLight)
+                .onTapGesture {
+                    show.toggle()
+                }
+            
+            BottomCardView()
+                .blur(radius: show ? __designTimeInteger("#1730.[1].[1].property.[0].[0].arg[0].value.[4].modifier[0].arg[0].value.then", fallback: 20) : __designTimeInteger("#1730.[1].[1].property.[0].[0].arg[0].value.[4].modifier[0].arg[0].value.else", fallback: 0))
+                .animation(.default, value: show)
+        }
+    
+#sourceLocation()
+    }
+}
+
+import struct SwiftUITestProject.ContentView
+import struct SwiftUITestProject.CardView
+import struct SwiftUITestProject.BackCardView
+import struct SwiftUITestProject.TitleView
+import struct SwiftUITestProject.BottomCardView
+#Preview {
+    ContentView()
+}
+
+
+
--- SwiftUITestProject/ContentView.swift
+++ SwiftUITestProject/ContentView.swift
@@ -8,17 +8,127 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var show = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            TitleView()
+                .blur(radius: show ? 20 : 0)
+                .animation(.easeInOut, value: show)
+            
+            BackCardView()
+                .background(show ? .card3 : .card4)
+                .cornerRadius(20)
+                .shadow(radius: 20)
+                .offset(x: 0, y: show ? -400 : -40)
+                .scaleEffect(0.9)
+                .rotationEffect(.degrees(show ? 0 : 10))
+                .rotation3DEffect(.degrees(10), axis: (x: 10.0, y: 0.0, z: 0.0))
+                .blendMode(.hardLight)
+                .animation(.easeInOut(duration: 0.5), value: show)
+            
+            BackCardView()
+                .background(show ? .card4 : .card3)
+                .cornerRadius(20)
+                .shadow(radius: 20)
+                .offset(x: 0, y: show ? -200 : -20)
+                .scaleEffect(0.95)
+                .rotationEffect(.degrees(show ? 0 : 5))
+                .rotation3DEffect(.degrees(5), axis: (x: 10.0, y: 0.0, z: 0.0))
+                .blendMode(.hardLight)
+                .animation(.easeInOut(duration: 0.3), value: show)
+            
+            CardView()
+                .blendMode(.hardLight)
+                .onTapGesture {
+                    show.toggle()
+                }
+            
+            BottomCardView()
+                .blur(radius: show ? 20 : 0)
+                .animation(.default, value: show)
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct CardView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("UI Design")
+                        .font(.title)
+                        .fontWeight(.semibold)
+                        .foregroundStyle(.white)
+                    Text("Certificate")
+                        .foregroundColor(Color("accent"))
+                }
+                Spacer()
+                Image("Logo1")
+            }
+            .padding(.horizontal, 20)
+            .padding(.top, 20)
+            Spacer()
+            Image("Card1")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 300, height: 110, alignment: .top)
+        }
+        .frame(width: 340.0, height: 220.0)
+        .background(.black)
+        .cornerRadius(20)
+        .shadow(radius: 20)
+    }
+}
+
+struct BackCardView: View {
+    var body: some View {
+        VStack {
+            Spacer()
+        }
+        .frame(width: 340, height: 220)
+    }
+}
+
+struct TitleView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Certificates")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                Spacer()
+            }
+            .padding()
+            Image(.background1)
+            Spacer()
+        }
+    }
+}
+
+struct BottomCardView: View {
+    var body: some View {
+        VStack(spacing: 20) {
+            Rectangle()
+                .frame(width: 40, height: 5)
+                .cornerRadius(3)
+                .opacity(0.1)
+            Text("This certificate is proof that Meng To has achieved the UI Design course with approval from a Design+Code instructor.")
+                .multilineTextAlignment(.center)
+                .font(.subheadline)
+                .lineSpacing(4)
+            Spacer()
+        }
+        .padding(.top, 8)
+        .padding(.horizontal, 20)
+        .frame(maxWidth: .infinity)
+        .background(.white)
+        .cornerRadius(30)
+        .shadow(radius: 20)
+        .offset(x: 0, y: 500)
+    }
 }

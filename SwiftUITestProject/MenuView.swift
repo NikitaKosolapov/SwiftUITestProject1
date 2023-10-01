f2ed3b004610489403189f63f1741f51118777d6
Nikita Kosolapov <kosolapovnikitav@gmail.com>
2023-10-01 10:14:07 +0300

    Added MenuView

---
+++ SwiftUITestProject/MenuView.swift
@@ -0,0 +1,51 @@
+//
+//  MenuView.swift
+//  SwiftUITestProject
+//
+//  Created by Nikita Kosolapov on 01.10.2023.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    var body: some View {
+        VStack {
+            Spacer()
+
+            VStack(spacing: 16) {
+                MenuRow(title: "Account", icon: "gear")
+                MenuRow(title: "Billing", icon: "creditcard")
+                MenuRow(title: "Sign out", icon: "person.crop.circle")
+            }
+            .frame(maxWidth: .infinity)
+            .frame(height: 300)
+            .background(.white)
+            .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
+            .shadow(radius: 30)
+        .padding(.horizontal, 30)
+        }
+        .padding(.bottom, 30)
+    }
+}
+
+#Preview {
+    MenuView()
+}
+
+struct MenuRow: View {
+    var title: String
+    var icon: String
+    
+    var body: some View {
+        HStack(spacing: 16) {
+            Image(systemName: icon)
+                .font(.system(size: 20, weight: .light))
+                .imageScale(.large)
+                .frame(width: 32, height: 32)
+            
+            Text(title)
+                .font(.system(size: 20, weight: .bold, design: .rounded))
+                .frame(width: 120, alignment: .leading)
+        }
+    }
+}
